4a8a9aed79470dea9b8154e83d39ff8bfd7b2df2
divyaraok <divyaraok29@gmail.com>
2020-08-13 22:40:19 +0530

    Prepared JSON output

--- useractivity/app/models.py
+++ useractivity/app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+
+# User Model: (id, first_name, last_name)
+class Users(models.Model):
+    # Fields
+    id = models.CharField(max_length=9, primary_key=True, default=uuid.uuid4().hex[:9].upper(), help_text='User Id - Primary Key')
+    first_name = models.CharField(max_length=20, help_text='First name of the user')
+    last_name = models.CharField(max_length=20, help_text='Last name of the user')
+
+    # Metadata
+    class Meta:
+        ordering = ['id']
+
+    # Methods
+    def __str__(self):
+        return str(self.id)
+
+    def get_absolute_url(self):
+        return reverse('full-name', args=[str(self.id)])
+
+
+# UserActivity Model: (activity_id, user_id, start_time, end_time)
+class UserActivity(models.Model):
+    # Fields
+    activity_id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Activity Id - Primary Key')
+    user_id = models.ForeignKey('Users', on_delete=models.SET_NULL, null=True, help_text='User Id from the Users model')
+    start_time = models.DateTimeField(null=True, blank=True, help_text='Start time of the activity')
+    end_time = models.DateTimeField(null=True, blank=True, help_text='End time of the activity')
+
+    # Metadata
+    class Meta:
+        ordering = ['-start_time', '-end_time']
+
+    # Methods
+    def __str__(self):
+        return str(self.user_id)
+
--- useractivity/app/views.py
+++ useractivity/app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Users, UserActivity
+from django.http import HttpResponse
+import json
+from collections import OrderedDict
 
-# Create your views here.
+
+def index(request):
+    useractivity = UserActivity.objects.all()
+    user = {}
+    for i in useractivity:
+        user[i.user_id] = Users.objects.filter(id=i.user_id)
+
+    result = OrderedDict()
+    members = []
+    for i in user:
+        t = OrderedDict()
+        t["id"] = i.id
+        t["real name"] = i.first_name + " " + i.last_name
+        ua = UserActivity.objects.filter(user_id=i.id)
+        l = []
+        for j in ua:
+            d = OrderedDict()
+            t["time zone"] = str(j.start_time.tzinfo)
+            d["start_time"] = str(j.start_time.strftime("%b %d %Y %I:%M %p"))
+            d["end_time"] = str(j.end_time.strftime("%b %d %Y %I:%M %p"))
+            l.append(d)
+        t["activity_periods"] = l
+        members.append(t)
+    result["ok"] = "true"
+    result["members"] = members
+    result = OrderedDict(result)
+
+    json_pretty = json.dumps(result, sort_keys=True, indent=4)
+    return HttpResponse(json_pretty, content_type="application/json")
+
+
